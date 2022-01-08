5bf7a4c9661122d8dde82fb881076f63a8f06d0c
William Moses <03williammoses@gmail.com>
2022-01-08 18:21:31 -0500

    Update

--- src/Assignment_3_part_2/AVLTree.java
+++ src/Assignment_3_part_2/AVLTree.java
@@ -1,223 +1,629 @@
+/*
+ * 
+ */
 package Assignment_3_part_2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
+// TODO: Auto-generated Javadoc
 class Node {
-	int key;
+	long key;
 	String value;
 	int h;
-	Node leftChild;
-	Node rightChild;
+	Node left;
+	Node right;
 
 	public Node() {
-		leftChild = null;
-		rightChild = null;
+		left = null;
+		right = null;
 		key = 0;
 		value = "NA";
 		h = 0;
 	}
 
-	public Node(int key) {
-		leftChild = null;
-		rightChild = null;
+	public Node(long key) {
+		left = null;
+		right = null;
 		this.key = key;
 		this.value = "ABC School";
 		h = 0;
 	}
 }
 
+/**
+ * The Class AVLTree.
+ */
 public class AVLTree extends CleverSIDC {
-	private Node rootNode;
 
-	// Constructor to set null value to the rootNode
-	public AVLTree(int totalNumberOfStudents) {
-		super(totalNumberOfStudents);
-		rootNode = null;
+	/** The root. */
+	private Node root;
+
+	/** The Stack array. */
+	Node StackArray[];
+
+	static long arr[];
+	static int index = 0;
+
+	/**
+	 * The Class Stack.
+	 */
+	class Stack {
+
+		/** The top. */
+		int top = -1;
+
+		/**
+		 * Instantiates a new stack.
+		 */
+		public Stack() {
+			top = -1;
+		}
+
+		/**
+		 * Push.
+		 *
+		 * @param data the data
+		 */
+		public void push(Node data) {
+			if (top == StackArray.length) {
+				System.out.println("Stack is FULL");
+			} else {
+				StackArray[++top] = data;
+			}
+		}
+
+		/**
+		 * Pop.
+		 *
+		 * @return the node
+		 */
+		public Node pop() {
+			if (top == -1) {
+				System.out.println("Stack is EMPTY");
+				return null;
+			} else {
+				return StackArray[top--];
+			}
+		}
 	}
 
+	/**
+	 * Instantiates a new AVL tree.
+	 *
+	 * @param totalNumberOfStudents the total number of students
+	 */
+	public AVLTree(int totalNumberOfStudents) {
+		super(totalNumberOfStudents);
+		System.out.println("AVL Tree has been loaded");
+		StackArray = new Node[this.totalNumberOfStudents];
+		arr = new long[this.totalNumberOfStudents];
+		root = null;
+	}
+
+	/**
+	 * Sets the SIDC threshold.
+	 *
+	 * @param Size the new SIDC threshold
+	 */
 	public void setSIDCThreshold(int Size) {
 		this.totalNumberOfStudents = Size;
 	}
 
-	public void generate() {
-		
+	/**
+	 * Checks if is exists.
+	 *
+	 * @param cleverSIDC the clever SIDC
+	 * @param num        the num
+	 * @return true, if is exists
+	 */
+	public boolean isExists(CleverSIDC cleverSIDC, int num) {
+		Stack s = new Stack();
+		Node curr = root;
+		while (curr != null || s.top > -1) {
+			while (curr != null) {
+				s.push(curr);
+				curr = curr.left;
+			}
+			curr = s.pop();
+			if (curr.key == num) {
+				return true;
+			}
+			curr = curr.right;
+		}
+		return false;
 	}
 
-	public List<Integer> allKeys(CleverSIDC ob) {
-		return inorderTraversal();
+	/**
+	 * Generate.
+	 *
+	 * @return the int
+	 */
+	public int generate() {
+		int max = 99999999;
+		int min = 10000000;
+		int range = max - min + 1;
+		boolean exists = false;
+		int number = 1;
+		while (!exists) {
+			number = (int) (Math.random() * range) + min;
+			exists = isExists(this, number);
+		}
+		return number;
 	}
 
+	/**
+	 * All keys.
+	 *
+	 * @param ob the ob
+	 * @return the string
+	 */
+	public long[] allKeys(CleverSIDC ob) {
+		return inOrderKeys();
+	}
+
+	/**
+	 * Adds the.
+	 *
+	 * @param ob    the ob
+	 * @param key   the key
+	 * @param value the value
+	 */
 	@Override
-	public void add(CleverSIDC ob, int key, String value) {
-		insertElement(key);
+	public void add(CleverSIDC ob, long key, String value) {
+		insert(key);
 	}
 
+	/**
+	 * Removes the.
+	 *
+	 * @param ob  the ob
+	 * @param key the key
+	 * @return the string
+	 */
 	@Override
-	public String remove(CleverSIDC ob, int key) {
-		return null;
+	public String remove(CleverSIDC ob, long key) {
+		return delete(key);
 	}
 
+	/**
+	 * Gets the values.
+	 *
+	 * @param ob  the ob
+	 * @param key the key
+	 * @return the values
+	 */
 	@Override
-	public String getValues(CleverSIDC ob, int key) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getValues(CleverSIDC ob, long key) {
+		return getValue(key);
 	}
 
+	/**
+	 * Next key.
+	 *
+	 * @param ob  the ob
+	 * @param key the key
+	 * @return the long
+	 */
 	@Override
-	public int nextKey(CleverSIDC ob, int key) {
-		// TODO Auto-generated method stub
-		return 0;
+	public long nextKey(CleverSIDC ob, long key) {
+		return getNextKey(key);
 	}
 
+	/**
+	 * Prev key.
+	 *
+	 * @param ob  the ob
+	 * @param key the key
+	 * @return the long
+	 */
 	@Override
-	public int prevKey(CleverSIDC ob, int key) {
-		// TODO Auto-generated method stub
-		return 0;
+	public long prevKey(CleverSIDC ob, long key) {
+		return getPrevKey(key);
 	}
 
+	/**
+	 * Range key.
+	 *
+	 * @param key1 the key 1
+	 * @param key2 the key 2
+	 * @return the long[]
+	 */
 	@Override
-	public List<Integer> rangeKey(int key1, int key2) {
-		// TODO Auto-generated method stub
-		return null;
+	public long[] rangeKey(long key1, long key2) {
+		return getRangeKey(key1, key2);
 	}
 
-	public void removeAll() {
-		rootNode = null;
+	/**
+	 * Height.
+	 *
+	 * @param N the n
+	 * @return the int
+	 */
+	int height(Node N) {
+		if (N == null)
+			return 0;
+		return N.h;
 	}
 
-	public boolean checkEmpty() {
-		if (rootNode == null)
-			return true;
-		else
-			return false;
+	/**
+	 * Max.
+	 *
+	 * @param a the a
+	 * @param b the b
+	 * @return the int
+	 */
+	int max(int a, int b) {
+		return (a > b) ? a : b;
 	}
 
-	public void insertElement(int element) {
-		rootNode = insertElement(element, rootNode);
+	/**
+	 * Right rotate.
+	 *
+	 * @param y the y
+	 * @return the node
+	 */
+	Node rightRotate(Node y) {
+		Node x = y.left;
+		Node T2 = x.right;
+
+		x.right = y;
+		y.left = T2;
+
+		y.h = max(height(y.left), height(y.right)) + 1;
+		x.h = max(height(x.left), height(x.right)) + 1;
+
+		return x;
 	}
 
-	private int getHeight(Node node) {
-		return node == null ? -1 : node.h;
+	/**
+	 * Left rotate.
+	 *
+	 * @param x the x
+	 * @return the node
+	 */
+	Node leftRotate(Node x) {
+		Node y = x.right;
+		Node T2 = y.left;
+
+		y.left = x;
+		x.right = T2;
+
+		x.h = max(height(x.left), height(x.right)) + 1;
+		y.h = max(height(y.left), height(y.right)) + 1;
+
+		return y;
 	}
 
-	private int getMaxHeight(int leftNodeHeight, int rightNodeHeight) {
-		return leftNodeHeight > rightNodeHeight ? leftNodeHeight : rightNodeHeight;
+	/**
+	 * Gets the balance.
+	 *
+	 * @param N the n
+	 * @return the balance
+	 */
+	int getBalance(Node N) {
+		if (N == null)
+			return 0;
+		return height(N.left) - height(N.right);
 	}
 
-	private Node insertElement(int element, Node node) {
+	/**
+	 * Insert.
+	 *
+	 * @param key the key
+	 * @return the node
+	 */
+	public Node insert(long key) {
+		this.root = insert(root, key);
+		return root;
+	}
+
+	/**
+	 * Insert.
+	 *
+	 * @param node the node
+	 * @param key  the key
+	 * @return the node
+	 */
+	Node insert(Node node, long key) {
 		if (node == null)
-			node = new Node(element);
-		
-		else if (element < node.key) {
-			node.leftChild = insertElement(element, node.leftChild);
-			if (getHeight(node.leftChild) - getHeight(node.rightChild) == 2)
-				if (element < node.leftChild.key)
-					node = rotateWithLeftChild(node);
-				else
-					node = doubleWithLeftChild(node);
-		} else if (element > node.key) {
-			node.rightChild = insertElement(element, node.rightChild);
-			if (getHeight(node.rightChild) - getHeight(node.leftChild) == 2)
-				if (element > node.rightChild.key)
-					node = rotateWithRightChild(node);
-				else
-					node = doubleWithRightChild(node);
-		} else
-			; 
-		node.h = getMaxHeight(getHeight(node.leftChild), getHeight(node.rightChild)) + 1;
+			return (new Node(key));
+
+		if (key < node.key)
+			node.left = insert(node.left, key);
+		else if (key > node.key)
+			node.right = insert(node.right, key);
+		else
+			return node;
+		node.h = 1 + max(height(node.left), height(node.right));
+
+		int balance = getBalance(node);
+
+		if (balance > 1 && key < node.left.key)
+			return rightRotate(node);
+
+		if (balance < -1 && key > node.right.key)
+			return leftRotate(node);
+
+		if (balance > 1 && key > node.left.key) {
+			node.left = leftRotate(node.left);
+			return rightRotate(node);
+		}
+
+		if (balance < -1 && key < node.right.key) {
+			node.right = rightRotate(node.right);
+			return leftRotate(node);
+		}
 
 		return node;
-
 	}
 
-	private Node rotateWithLeftChild(Node node2) {
-		Node node1 = node2.leftChild;
-		node2.leftChild = node1.rightChild;
-		node1.rightChild = node2;
-		node2.h = getMaxHeight(getHeight(node2.leftChild), getHeight(node2.rightChild)) + 1;
-		node1.h = getMaxHeight(getHeight(node1.leftChild), node2.h) + 1;
-		return node1;
+	/**
+	 * Min value node.
+	 *
+	 * @param node the node
+	 * @return the node
+	 */
+	Node minValueNode(Node node) {
+		Node current = node;
+
+		while (current.left != null)
+			current = current.left;
+
+		return current;
 	}
 
-	private Node rotateWithRightChild(Node node1) {
-		Node node2 = node1.rightChild;
-		node1.rightChild = node2.leftChild;
-		node2.leftChild = node1;
-		node1.h = getMaxHeight(getHeight(node1.leftChild), getHeight(node1.rightChild)) + 1;
-		node2.h = getMaxHeight(getHeight(node2.rightChild), node1.h) + 1;
-		return node2;
+	/**
+	 * Delete.
+	 *
+	 * @param key the key
+	 * @return the string
+	 */
+	public String delete(long key) {
+		return deleteNode(root, key) != null ? "Key " + key + " removed successfully" : "Key not found";
 	}
 
-	private Node doubleWithLeftChild(Node node3) {
-		node3.leftChild = rotateWithRightChild(node3.leftChild);
-		return rotateWithLeftChild(node3);
-	}
-	
-	private Node doubleWithRightChild(Node node1) {
-		node1.rightChild = rotateWithLeftChild(node1.rightChild);
-		return rotateWithRightChild(node1);
-	}
-
-	public int getTotalNumberOfNodes() {
-		return getTotalNumberOfNodes(rootNode);
-	}
-
-	private int getTotalNumberOfNodes(Node head) {
-		if (head == null)
-			return 0;
+	/**
+	 * Delete node.
+	 *
+	 * @param root the root
+	 * @param key  the key
+	 * @return the node
+	 */
+	Node deleteNode(Node root, long key) {
+		if (root == null)
+			return root;
+		if (key < root.key)
+			root.left = deleteNode(root.left, key);
+		else if (key > root.key)
+			root.right = deleteNode(root.right, key);
 		else {
-			int length = 1;
-			length = length + getTotalNumberOfNodes(head.leftChild);
-			length = length + getTotalNumberOfNodes(head.rightChild);
-			return length;
-		}
-	}
+			if ((root.left == null) || (root.right == null)) {
+				Node temp = null;
+				if (temp == root.left)
+					temp = root.right;
+				else
+					temp = root.left;
 
-	public boolean searchElement(int element) {
-		return searchElement(rootNode, element);
-	}
+				if (temp == null) {
+					temp = root;
+					root = null;
+				} else
+					root = temp;
+			} else {
 
-	private boolean searchElement(Node head, int element) {
-		boolean check = false;
-		while ((head != null) && !check) {
-			int headElement = head.key;
-			if (element < headElement)
-				head = head.leftChild;
-			else if (element > headElement)
-				head = head.rightChild;
-			else {
-				check = true;
-				break;
+				Node temp = minValueNode(root.right);
+
+				root.key = temp.key;
+
+				root.right = deleteNode(root.right, temp.key);
 			}
-			check = searchElement(head, element);
 		}
-		return check;
-	}
-	public List<Integer> inorderTraversal() {
-		return inorderTraversal(rootNode);
+
+		if (root == null)
+			return root;
+
+		root.h = max(height(root.left), height(root.right)) + 1;
+
+		int balance = getBalance(root);
+
+		if (balance > 1 && getBalance(root.left) >= 0)
+			return rightRotate(root);
+
+		if (balance > 1 && getBalance(root.left) < 0) {
+			root.left = leftRotate(root.left);
+			return rightRotate(root);
+		}
+
+		if (balance < -1 && getBalance(root.right) <= 0)
+			return leftRotate(root);
+
+		if (balance < -1 && getBalance(root.right) > 0) {
+			root.right = rightRotate(root.right);
+			return leftRotate(root);
+		}
+
+		return root;
 	}
 
-	private List<Integer> inorderTraversal(Node head) {
-		Stack<Node> s = new Stack<Node>();
-		List<Integer> list = new ArrayList<>();
-        Node curr = head;
- 
-        while (curr != null || s.size() > 0)
-        {
-            while (curr !=  null)
-            {
-                s.push(curr);
-                curr = curr.leftChild;
-            }
-            curr = s.pop(); 
-            list.add(curr.key);
-            curr = curr.rightChild;
-        }
-		return list;
-		
+	/**
+	 * Gets the range key.
+	 *
+	 * @param r1 the r 1
+	 * @param r2 the r 2
+	 * @return the range key
+	 */
+	public long[] getRangeKey(long r1, long r2) {
+		return getRangeKey(root, r1, r2);
+	}
+
+	/**
+	 * Gets the range key.
+	 *
+	 * @param head the head
+	 * @param r1   the r 1
+	 * @param r2   the r 2
+	 * @return the range key
+	 */
+	private long[] getRangeKey(Node head, long r1, long r2) {
+		Node curr = head;
+		Stack s = new Stack();
+		long[] listArray = new long[this.totalNumberOfStudents];
+		int index = 0;
+		while (curr != null || s.top > -1) {
+			while (curr != null) {
+				s.push(curr);
+				curr = curr.left;
+			}
+			curr = s.pop();
+			if (curr.key == r1) {
+				listArray[index++] = curr.key;
+			} else if (index != 0) {
+				listArray[index++] = curr.key;
+			}
+			if (curr.key == r2) {
+				return listArray;
+			}
+			curr = curr.right;
+		}
+		return listArray;
+	}
+
+	/**
+	 * Gets the prev key.
+	 *
+	 * @param key the key
+	 * @return the prev key
+	 */
+	public long getPrevKey(long key) {
+		return getPrevKey(root, key);
+	}
+
+	/**
+	 * Gets the prev key.
+	 *
+	 * @param head the head
+	 * @param key  the key
+	 * @return the prev key
+	 */
+	private long getPrevKey(Node head, long key) {
+		Node curr = head;
+		Stack s = new Stack();
+		long temp = curr.key;
+		while (curr != null || s.top > -1) {
+			while (curr != null) {
+				s.push(curr);
+				curr = curr.left;
+			}
+			curr = s.pop();
+			if (curr.key == key) {
+				return temp;
+			} else {
+				temp = curr.key;
+			}
+			curr = curr.right;
+		}
+		return temp;
+	}
+
+	/**
+	 * Gets the next key.
+	 *
+	 * @param key the key
+	 * @return the next key
+	 */
+	public long getNextKey(long key) {
+		return getNextKey(root, key);
+	}
+
+	/**
+	 * Gets the next key.
+	 *
+	 * @param head the head
+	 * @param key  the key
+	 * @return the next key
+	 */
+	private long getNextKey(Node head, long key) {
+		Node curr = head;
+		Stack s = new Stack();
+		long temp = curr.key;
+		while (curr != null || s.top > -1) {
+			while (curr != null) {
+				s.push(curr);
+				curr = curr.left;
+			}
+			curr = s.pop();
+			if (temp == key) {
+				return curr.key;
+			} else {
+				temp = curr.key;
+			}
+			curr = curr.right;
+		}
+		return temp;
+	}
+
+	/**
+	 * Gets the value.
+	 *
+	 * @param key the key
+	 * @return the value
+	 */
+	public String getValue(long key) {
+		return getValue(root, key);
+	}
+
+	/**
+	 * Gets the value.
+	 *
+	 * @param head the head
+	 * @param key  the key
+	 * @return the value
+	 */
+	private String getValue(Node head, long key) {
+		Node curr = head;
+		Stack s = new Stack();
+
+		while (curr != null || s.top > -1) {
+			while (curr != null) {
+				s.push(curr);
+				curr = curr.left;
+			}
+			curr = s.pop();
+			if (curr.key == key) {
+				return curr.value;
+			}
+			curr = curr.right;
+		}
+		return "Not found";
+	}
+
+	/**
+	 * In order keys.
+	 *
+	 * @return the string
+	 */
+	public long[] inOrderKeys() {
+		arr = new long[this.totalNumberOfStudents];
+		index = 0;
+		long start = System.currentTimeMillis();
+		inorder(root);
+		long end = System.currentTimeMillis();
+		System.out.println("Time took to sort allKeys():" + (end - start) + "ms");
+		return arr;
+	}
+
+	/**
+	 * In order keys.
+	 *
+	 * @param head the head
+	 * @return the string
+	 */
+	void inorder(Node node) {
+		if (node == null)
+			return;
+		inorder(node.left);
+		arr[index++] = node.key;
+		inorder(node.right);
 	}
 
 	
+
 }
--- src/Assignment_3_part_2/NASTA.java
+++ src/Assignment_3_part_2/NASTA.java
@@ -1,12 +1,14 @@
 package Assignment_3_part_2;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
- 	
 
+// TODO: Auto-generated Javadoc
 abstract class CleverSIDC {
 	int totalNumberOfStudents;
 
@@ -16,49 +18,98 @@ abstract class CleverSIDC {
 
 	abstract void setSIDCThreshold(int Size);
 
-	abstract void generate();
+	abstract int generate();
 
-	abstract List<Integer> allKeys(CleverSIDC ob);
+	abstract long[] allKeys(CleverSIDC ob);
 
-	abstract void add(CleverSIDC ob, int key, String value);
+	abstract void add(CleverSIDC ob, long key, String value);
 
-	abstract String remove(CleverSIDC ob, int key);
+	abstract String remove(CleverSIDC ob, long key);
 
-	abstract String getValues(CleverSIDC ob, int key);
+	abstract String getValues(CleverSIDC ob, long key);
 
-	abstract int nextKey(CleverSIDC ob, int key);
+	abstract long nextKey(CleverSIDC ob, long key);
 
-	abstract int prevKey(CleverSIDC ob, int key);
+	abstract long prevKey(CleverSIDC ob, long key);
 
-	abstract List<Integer> rangeKey(int key1, int key2);
+	abstract long[] rangeKey(long key1, long key2);
 }
 
+/**
+ * The Class NASTA.
+ */
 public class NASTA {
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws FileNotFoundException the file not found exception
+	 */
 	public static void main(String args[]) throws FileNotFoundException {
-		Scanner ob = new Scanner(new FileInputStream("Resources/NASTA_Files/NASTA_test_file1.txt"));
-		List<Integer> listLong = new ArrayList<>();
-		while(ob.hasNextInt()) {
+		Scanner s = new Scanner(System.in);
+		CleverSIDC c = null;
+		System.out.println("Welcome to SIDC");
+		System.out.println("List of files available to read");
+		File folder = new File("Resources/NASTA_Files/");
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				System.out.println("File " + listOfFiles[i].getName());
+			} else if (listOfFiles[i].isDirectory()) {
+				System.out.println("Directory " + listOfFiles[i].getName());
+			}
+		}
+		String fileName = s.next();
+		Scanner ob = new Scanner(new FileInputStream("Resources/NASTA_Files/" + fileName));
+		System.out.println(fileName + " has been loaded and ready to read!");
+		List<Integer> listLong = new ArrayList<>();		
+		while (ob.hasNextInt()) {
 			listLong.add(ob.nextInt());
 		}
-		if(listLong.size()>100) {
-			CleverSIDC c = new AVLTree(listLong.size());
-			for(int key:listLong) {
-				c.add(c, key, "School Name");
-			}
-			List<Integer> list = c.allKeys(c);
-			for(int key:list) {
-				System.out.println(key);
-			}
+		System.out.println("The Threshold size has set to - "+listLong.size() );
+
+		if (listLong.size() > 1000) {
+			c = new AVLTree(listLong.size());
 		} else {
-			CleverSIDC c = new AVLTree(listLong.size());
-			for(int key:listLong) {
-				c.add(c, key, "School Name");
-			}System.out.println();
-			List<Integer> list = c.allKeys(c);
-			for(int key:list) {
-				System.out.println(key);
+			c = new LinkedList(listLong.size());
+		}
+		for (int key : listLong) {
+			c.add(c, key, "School Name");
+		}
+		System.out.println("List loaded successfully!!!\n");	
+		
+		
+		long start = System.currentTimeMillis();
+		System.out.println(Arrays.toString(c.allKeys(c)));
+		long end = System.currentTimeMillis();
+		System.out.println("Time took to print allKeys():" + ((end - start)/1000) + "s\n");
+		
+		long gen = c.generate();
+		System.out.println("generate(): " + gen);
+		c.add(c, gen, "School Name");
+		
+		start = System.currentTimeMillis();
+		System.out.println(Arrays.toString(c.allKeys(c)));
+		end = System.currentTimeMillis();
+		System.out.println("Time took to print allKeys():" + ((end - start)/1000) + "s\n");
+		
+		System.out.println("remove(3326261): " + c.remove(c, 3326261));
+		System.out.println(Arrays.toString(c.allKeys(c)));
+		System.out.println("getValues(84629786): " + c.getValues(c, 84629786));
+		System.out.println("prevKey(84629786): " + c.prevKey(c, 84629786));
+		System.out.println("nextKey(84629786): " + c.nextKey(c, 84629786));
+		System.out.print("rangeKey(): ");
+		long[] arr = c.rangeKey(70188256, 84629786);
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=0) {
+				System.out.print(arr[i]+", ");
+			}else {
+				break;
 			}
 		}
+
 		ob.close();
 	}
 }
